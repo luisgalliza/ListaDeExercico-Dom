9a8dd0e2a993ec79b1a5a2957b044e4868f9e053
Luis Galliza <luiscbggomes@gmail.com>
2025-11-13 20:48:54 -0300

    Add files via upload

---
+++ AtividadeHTML-JS/JS/questao01.js
@@ -0,0 +1,9 @@
+function inserirTitulo() {
+
+    let texto = document.getElementById("txt-titulo").value;
+
+    let titulo = document.getElementById("titulo");
+    titulo.innerText = texto;
+
+    titulo.style.color = "blue";
+}
---
+++ AtividadeHTML-JS/JS/questao03.js
@@ -0,0 +1,16 @@
+function cadastrarUsuario() {
+    let nome = document.getElementById("nome").value;
+    let dataNasc = document.getElementById("dataNasc").value;
+    let rua = document.getElementById("rua").value;
+    let numero = document.getElementById("numero").value;
+    let cidade = document.getElementById("cidade").value;
+    let uf = document.getElementById("uf").value;
+
+    let frase = `${nome} nasceu no dia ${dataNasc} e mora na rua ${rua}, número ${numero}, na cidade ${cidade} do estado ${uf}.`;
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerText = frase;
+
+    resultado.style.color = "green";
+    resultado.style.fontSize = "24px";
+}
